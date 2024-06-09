c06f2137e1b173547a7a5bdc407bd80d985eb39e
unknown <shilpas2101@gmail.com>
2024-06-09 12:02:08 +0530

    uploaded

---
+++ src/components/AddShop.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import Navbar from './Navbar'
+
+const AddShop = () => {
+    const [data,setData]=useState(
+        {
+            "name": "",
+            "username": "",
+            "email": "",
+            "phone": ""
+
+        }
+    )
+    const inputHandler = (event)=>{
+        setData({...data,[event.target.name]:event.target.value })
+    }
+    const readValue=()=>{
+        console.log(data)
+    }
+  return (
+    <div>
+        <Navbar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Name</label>
+                            <input type="text" className="form-control" name='name' value={data.name} onChange={inputHandler}/>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Username</label>
+                            <input type="text" className="form-control" name='username' value={data.username} onChange={inputHandler} />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Email</label>
+                            <input type="email" name="email" id="" className="form-control"  value={data.email} onChange={inputHandler} />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Phone</label>
+                            <input type="text" className="form-control" name='phone' value={data.phone} onChange={inputHandler} />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn-primary" onClick={readValue}>Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddShop
